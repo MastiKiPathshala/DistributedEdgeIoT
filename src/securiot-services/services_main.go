b6310ba32e547754a7d58143fcc668ba4f0fc190
srinibas <srinibas@gmail.com>
2017-06-23 05:11:50 -0400

    mqtt message receive

--- src/securiot-services/services_main.go
+++ src/securiot-services/services_main.go
@@ -13,3 +13,125 @@
  *
  ************************************************************************/
 
+package main
+
+import (
+
+   "fmt"
+/*
+    "io/ioutil"
+    "encoding/json"
+   "os/exec"
+   "sync"
+   "time"
+*/
+   "flag"
+   "os"
+   MQTT "github.com/eclipse/paho.mqtt.golang"
+   "github.com/op/go-logging"
+)
+
+var log = logging.MustGetLogger("example")
+
+var format = logging.MustStringFormatter(
+   `%{color}%{time:15:04:05.000} %{shortfunc} ? %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+)
+
+type Password string
+
+func (p Password) Redacted() interface{} {
+   return logging.Redact(string(p))
+}
+
+var client MQTT.Client
+var topic *string
+
+func main () {
+
+   backend1 := logging.NewLogBackend(os.Stderr, "", 0)
+   backend2 := logging.NewLogBackend(os.Stderr, "", 0)
+
+   backend2Formatter := logging.NewBackendFormatter(backend2, format)
+
+   backend1Leveled := logging.AddModuleLevel(backend1)
+   backend1Leveled.SetLevel(logging.INFO, "")
+
+   logging.SetBackend(backend1Leveled, backend2Formatter)
+
+   SubscribeMqtt ()
+
+}
+
+func SubscribeMqtt () {
+
+   topic  := flag.String("topic", "gps-data", "The topic name to/from which to publish/subscribe")
+   broker := flag.String("broker", "tcp://localhost:1883", "The broker URI. ex: tcp://10.10.1.1:1883")
+
+   id := flag.String("id", "testgoid", "The ClientID (optional)")
+   cleansess := flag.Bool("clean", false, "Set Clean Session (default false)")
+
+   qos := flag.Int("qos", 0, "The Quality of Service 0,1,2 (default 0)")
+
+   action := flag.String("action", "sub", "Action publish or subscribe (required)")
+   store  := flag.String("store", ":memory:", "The Store Directory (default use memory store)")
+
+   flag.Parse()
+
+   if *topic == "" {
+      fmt.Println("Invalid setting for -topic, must not be empty")
+      return
+   }
+
+   if *action != "pub" && *action != "sub" {
+      fmt.Println("Invalid setting for -action, must be pub or sub")
+      return
+   }
+
+   opts := MQTT.NewClientOptions()
+   opts.AddBroker(*broker)
+
+   opts.SetClientID(*id)
+   opts.SetCleanSession(*cleansess)
+
+   if *store != ":memory:" {
+
+      opts.SetStore(MQTT.NewFileStore(*store))
+   }
+
+   if *action == "pub" {
+
+      client = MQTT.NewClient(opts)
+
+      if token := client.Connect(); token.Wait() && token.Error() != nil {
+
+         panic(token.Error())
+
+      }
+      fmt.Println("Sample Publisher Started")
+
+   } else {
+
+      if *action == "sub" {
+
+         opts.SetDefaultPublishHandler(ReadMessage)
+         
+         client := MQTT.NewClient(opts)
+         
+         if token := client.Connect(); token.Wait() && token.Error() != nil {
+         
+            panic(token.Error())
+         }
+         
+         if token := client.Subscribe(*topic, byte(*qos), nil); token.Wait() && token.Error() != nil {
+         
+            fmt.Println(token.Error())
+            os.Exit(1)
+         
+         }
+      }
+   }
+}
+
+var ReadMessage MQTT.MessageHandler = func (client MQTT.Client, msg MQTT.Message) {
+   fmt.Println ( msg.Topic(),string(msg.Payload()) )
+}
